7c75ba93b7cc578136f20b15428b6498279cae11
Anas Farooq <anasfarooq2006@gmail.com>
2025-01-09 03:21:08 +0500

    Added Team Section

---
+++ src/slices/TeamGrid/index.tsx
@@ -0,0 +1,45 @@
+import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import React, { JSX } from "react";
+import { Skater } from "./Skater";
+import { createClient } from "@/prismicio";
+import { Content } from "@prismicio/client";
+
+/**
+ * Props for `TeamGrid`.
+ */
+export type TeamGridProps = SliceComponentProps<Content.TeamGridSlice>;
+
+/**
+ * Component for "TeamGrid" Slices.
+ */
+const TeamGrid = async ({ slice }: TeamGridProps): Promise<JSX.Element> => {
+  const client = createClient()
+  const skaters = await client.getAllByType("skater");
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-navy"
+    >
+      <Heading as="h2" size="lg" className="mb-8 text-center text-white">
+      <PrismicText field={slice.primary.heading} />
+
+      </Heading>
+
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+        {skaters.map((skater, index) => (
+          <React.Fragment key={index}>
+{
+  skater.data.first_name && <Skater index={index} skater={skater}/>
+}
+
+          </React.Fragment>
+        ))}
+      </div>
+    </Bounded>
+  );
+};
+
+export default TeamGrid;

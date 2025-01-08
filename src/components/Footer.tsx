1557e658804e71d522d65d8b17728d5ee54dd227
Anas Farooq <anasfarooq2006@gmail.com>
2025-01-09 04:56:11 +0500

    Added footer physics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,8 @@ import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
 import React from 'react'
 import { Logo } from './Logo'
 import { Bounded } from './Bounded'
+import { FooterPhysics } from './Footerphysics'
+import { asImageSrc } from '@prismicio/client'
 
 type Props = {}
 
@@ -10,13 +12,21 @@ export async function Footer({}: Props) {
   const client = createClient()
   const settings = await client.getSingle("settings")
 
+  const boardTextureURLs = settings.data.footer_skateboards.map((item) => asImageSrc(item.skateboard, {h:600})).filter((url): url is string => Boolean(url))
+
   const currentYear = new Date().getFullYear()
 
   return (
     <footer className='bg-texture bg-zinc-900 text-white overflow-hidden'>
       <div className="relative h-[75vh] ~p-10/16 md:aspect-auto">
         <PrismicNextImage field={settings.data.footer_image} alt='' fill className='object-cover' width={1200} />
+
         {/* Physics boards */}
+
+<FooterPhysics boardTextureURLs={boardTextureURLs} className='absolute inset-0 overflow-hidden'>
+
+</FooterPhysics>
+
         {/* Logo */}
         <Logo className='pointer-events-none relative h-20 mix-blend-exclusion md:h-28' />
       </div>

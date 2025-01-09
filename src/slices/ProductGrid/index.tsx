6d96304004a39b6e3c90c3e2a635825fa584ec75
Anas Farooq <anasfarooq2006@gmail.com>
2025-01-09 05:23:20 +0500

    Added SlideIn Animation

--- src/slices/ProductGrid/index.tsx
+++ src/slices/ProductGrid/index.tsx
@@ -5,6 +5,7 @@ import { JSX } from "react";
 
 import { Bounded } from "@/components/Bounded";
 import { SkateboardProduct } from "./SkateboardProduct";
+import { SlideIn } from "@/components/SlideIn";
 /**
  * Props for `ProductGrid`.
  */
@@ -19,14 +20,16 @@ const ProductGrid = ({ slice }: ProductGridProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     className="bg-texture bg-brand-gray">
+      <SlideIn>
       <Heading className="text-center ~mb-4/6" as="h2">
 <PrismicText field={slice.primary.heading} />
 </Heading>
-
+</SlideIn>
+<SlideIn>
 <div className="text-center ~mb-6/10">
 <PrismicRichText field={slice.primary.body} />
 </div>
-
+</SlideIn>
 <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
 
 {slice.primary.product.map(({ skateboard }) => (
